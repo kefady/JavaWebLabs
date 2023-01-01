a8408c00bdb9447279fc5e989ee2b379a5d51f11
Kiril <55844647+kefady@users.noreply.github.com>
2023-01-01 11:23:31 +0200

    add test

--- src/test/java/ControllerTest.java
+++ src/test/java/ControllerTest.java
@@ -1,6 +1,7 @@
 import com.universityadmissions.controller.Command;
 import com.universityadmissions.controller.CommandEnum;
 import com.universityadmissions.controller.command.LoginCommand;
+import com.universityadmissions.controller.command.ProfileCommand;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,12 @@ public class ControllerTest {
         String command = "LOGIN";
         Command loginCommand = CommandEnum.valueOf(command).getCommand();
         Assertions.assertEquals(LoginCommand.class, loginCommand.getClass());
+    }
+
+    @Test
+    public void profileCommand() {
+        String command = "PROFILE";
+        Command profileCommand = CommandEnum.valueOf(command).getCommand();
+        Assertions.assertEquals(ProfileCommand.class, profileCommand.getClass());
     }
 }
